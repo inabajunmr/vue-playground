1833c4fef95ffd36cfee9582ecfd517bd08e0925
inabajunmr <inabajun.for.regi@gmail.com>
2018-04-19 01:14:50 +0900

    instance

---
+++ instance/app.js
@@ -0,0 +1,25 @@
+var data = { a: 1 }
+var vm = new Vue({
+  el: '#example',
+  data: data
+})
+
+vm.$data === data // => true
+vm.$el === document.getElementById('example') // => true
+
+// $watch はインスタンスメソッドです
+vm.$watch('a', function (newValue, oldValue) {
+   // このコールバックは `vm.a` の値が変わる時に呼ばれます
+   console.log("newValue")
+})
+
+
+new Vue({
+    data: {
+      a: 1
+    },
+    created: function () {
+      // `this` は vm インスタンスを指します
+      console.log('a is: ' + this.a)
+    }
+  })
