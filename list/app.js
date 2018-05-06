06c5497ba0673ad86d24cf42d36097a7d3265605
inabajunmr <inabajun.for.regi@gmail.com>
2018-05-06 19:04:59 +0900

    v-for

---
+++ list/app.js
@@ -0,0 +1,27 @@
+var vm = new Vue({
+  el: '#app',
+  data:{
+    items : [
+      {name : 'item1', id:1},
+      {name : 'item2', id:2},
+    ],
+    item: {
+      key1: "value1",
+      key2: "value2",
+      key3: "value3",
+    },
+    test:"test",
+    nest: {
+
+    },
+    filterValues:[1,2,3,4,5]
+  },
+  computed:{
+    filterNumbers : function() {
+      this.filterValues.filter(function(number){
+        return number == 4;
+      })
+    }
+  }
+})
+
