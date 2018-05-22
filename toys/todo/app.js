f0358cf3ff6d1e8a18ca4491064eae329c93620d
inabajunmr <inabajun.for.regi@gmail.com>
2018-05-22 09:39:32 +0900

    draw by vue

--- toys/todo/app.js
+++ toys/todo/app.js
@@ -1,18 +1,19 @@
 // Definition components
 // Task
 Vue.component('task', {
-    props:["title"],
+    props:["title", "done"],
     template: `
     <div class="row task">
-        <div class="11 col"><h4>{{ title }}</h4></div>
-        <div class="1 col"><button class="btn primary">Add task</button></div>
-        <div class="1 col"><button class="btn primary">Remove Card</button></div>
+        <div class="1 col"><button class="btn" v-bind:class="{ done : done, primary : !done }">DONE</button></div>
+        <div class="1 col"><button class="btn">Edit</button></div>
+        <div class="1 col"><button class="btn">Delete</button></div>
+        <div class="11 col"><h4>{{ title }}<hr v-bind:class="{ done : done }"></h4></div>
     </div>`
 })
 
 // Card
 Vue.component('card', {
-    props:["title"],
+    props:["title", "tasks"],
     template: `
     <div class="card">
     <div class="row">
@@ -20,7 +21,7 @@ Vue.component('card', {
         <div class="1 col"><button class="btn primary">Add task</button></div>
         <div class="1 col"><button class="btn primary">Remove Card</button></div>
     </div>
-    <task></task>
+    <task v-for='task in tasks' v-bind:title="task.title" v-bind:done="task.done"></task>
     </div>`
 })
 
@@ -28,7 +29,23 @@ Vue.component('card', {
 vm = new Vue({ 
     el: '#components-todo',
     data: {
-    }
+        cards: [
+            {
+                tasks: [
+                    {title:"クレカ止める", done: true},
+                    {title:"再発行する", done: false}
+                ],
+                title: "クレカ無くした"
+            },
+            {
+                tasks: [
+                    {title:"いっぱい頑張る", done: false},
+                    {title:"すごい頑張る", done: false}
+                ],
+                title: "頑張る"
+            }
+    ]
+      }
 })
   
   
