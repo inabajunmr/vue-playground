2666d23c92a36640d969f9642b00ad89b8534fa6
inabajunmr <inabajun.for.regi@gmail.com>
2018-05-23 09:33:16 +0900

    blur

--- toys/todo/app.js
+++ toys/todo/app.js
@@ -5,10 +5,9 @@ Vue.component('task', {
     template: `
     <div class="row task">
         <div class="1 col"><button class="btn" v-bind:class="{ done : task.done, primary : !task.done }" @click="ttask.done=!ttask.done">DONE</button></div>
-        <div class="1 col"><button class="btn" @click="edit=!edit">Edit</button></div>
         <div class="1 col"><button class="btn">Delete</button></div>
-        <div class="11 col" v-show="!edit"><h4>{{ task.title }}<hr v-bind:class="{ done : task.done }"></h4></div>
-        <div class="11 col" v-show="edit"><input class="card w-100" v-model="ttask.title"><hr v-bind:class="{ done : task.done }"></div>
+        <div class="11 col" v-show="!edit" @click="edit=!edit"><h4>{{ task.title }}<hr v-bind:class="{ done : task.done }"></h4></div>
+        <div class="11 col" v-show="edit"><input class="card w-100" v-model="ttask.title" @blur="edit=!edit"><hr v-bind:class="{ done : task.done }"></div>
     </div>`,
     data: function() {
         return {
@@ -24,12 +23,19 @@ Vue.component('card', {
     template: `
     <div class="card">
     <div class="row">
-        <div class="11 col"><h4>{{ card.title }}</h4></div>
+        <div class="11 col" v-show="!edit" @click="edit=!edit"><h4>{{ card.title }}</h4></div>
+        <div class="11 col" v-show="edit"><input class="card w-100" v-model="tcard.title" @blur="edit=!edit"></div>
         <div class="1 col"><button class="btn primary">Add task</button></div>
         <div class="1 col"><button class="btn primary">Remove Card</button></div>
     </div>
     <task v-for='task in card.tasks' v-bind:task="task" :key="task.id"></task>
-    </div>`
+    </div>`,
+    data: function() {
+        return {
+            tcard: this.card,
+            edit: false
+        }
+    }
 })
 
 // Vue
