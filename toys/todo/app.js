51e0df74763ab426364cbe6d3ed51ea3c24cd1c4
inabajunmr <inabajun.for.regi@gmail.com>
2018-05-25 03:26:49 +0900

    add task

--- toys/todo/app.js
+++ toys/todo/app.js
@@ -1,20 +1,21 @@
 // Definition components
 // Task
 Vue.component('task', {
-    props:["task"],
+    props:["task", "card"],
     template: `
     <div class="row task">
         <div class="1 col"><button class="btn" v-bind:class="{ done : task.done, primary : !task.done }" @click="ttask.done=!ttask.done">DONE</button></div>
         <div class="1 col"><button class="btn">Delete</button></div>
-        <div class="11 col" v-show="!edit" @click="edit=!edit"><h4>{{ task.title }}<hr v-bind:class="{ done : task.done }"></h4></div>
-        <div class="11 col" v-show="edit"><input class="card w-100" v-model="ttask.title" @blur="edit=!edit"><hr v-bind:class="{ done : task.done }"></div>
+        <div class="11 col" v-show="!edit && ttask.title != ''" @click="edit=!edit"><h4>{{ task.title }}<hr v-bind:class="{ done : task.done }"></h4></div>
+        <div class="11 col" v-show="edit || ttask.title == ''"><input class="card w-100" v-model="ttask.title" @blur="edit=!edit"><hr v-bind:class="{ done : task.done }"></div>
     </div>`,
     data: function() {
         return {
             ttask: this.task,
+            tcard: this.card,
             edit: false
         }
-    }
+    },
 })
 
 // Card
@@ -25,18 +26,25 @@ Vue.component('card', {
     <div class="row">
         <div class="11 col" v-show="!edit" @click="edit=!edit"><h4>{{ card.title }}</h4></div>
         <div class="11 col" v-show="edit"><input class="card w-100" v-model="tcard.title" @blur="edit=!edit"></div>
-        <div class="1 col"><button class="btn primary">Add task</button></div>
+        <div class="1 col"><button class="btn primary" @click="addTask">Add task</button></div>
         <div class="1 col"><button class="btn primary">Remove Card</button></div>
     </div>
-    <task v-for='task in card.tasks' v-bind:task="task" :key="task.id"></task>
+    <task v-for='task in card.tasks' v-bind:card="card" v-bind:task="task" :key="task.id"></task>
     </div>`,
     data: function() {
         return {
             tcard: this.card,
             edit: false
         }
+    },
+    methods: {
+        addTask: function() {
+            this.tcard.tasks.unshift({title:"", done: false, id: taskId++})
+        }
     }
 })
+
+taskId = 0;
 
 // Vue
 vm = new Vue({ 
@@ -45,15 +53,15 @@ vm = new Vue({
         cards: [
             {
                 tasks: [
-                    {title:"クレカ止める", done: true},
-                    {title:"再発行する", done: false}
+                    {title:"クレカ止める", done: true, id: taskId++},
+                    {title:"再発行する", done: false, id: taskId++}
                 ],
                 title: "クレカ無くした"
             },
             {
                 tasks: [
-                    {title:"いっぱい頑張る", done: false},
-                    {title:"すごい頑張る", done: false}
+                    {title:"いっぱい頑張る", done: false, id: taskId++},
+                    {title:"すごい頑張る", done: false, id: taskId++}
                 ],
                 title: "頑張る"
             }
