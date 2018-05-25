4d5be2eba1c9446d5a5038e3d96a3c93a11ad46b
inabajunmr <inabajun.for.regi@gmail.com>
2018-05-26 00:00:53 +0900

    fix initial blur bug

--- toys/todo/app.js
+++ toys/todo/app.js
@@ -6,14 +6,14 @@ Vue.component('task', {
     <div class="row task">
         <div class="1 col"><button class="btn" v-bind:class="{ done : task.done, primary : !task.done }" @click="ttask.done=!ttask.done">DONE</button></div>
         <div class="1 col"><button class="btn" @click="deleteTask">Delete</button></div>
-        <div class="11 col" v-show="!edit && ttask.title != ''" @click="edit=!edit"><h4>{{ task.title }}<hr v-bind:class="{ done : task.done }"></h4></div>
-        <div class="11 col" v-show="edit || ttask.title == ''"><input class="card w-100" v-model="ttask.title" @blur="edit=!edit"><hr v-bind:class="{ done : task.done }"></div>
+        <div class="11 col" v-show="!edit" @click="edit=!edit"><h4>{{ task.title }}<hr v-bind:class="{ done : task.done }"></h4></div>
+        <div class="11 col" v-show="edit"><input class="card w-100" v-model="ttask.title" @blur="edit=!edit"><hr v-bind:class="{ done : task.done }"></div>
     </div>`,
     data: function() {
         return {
             ttask: this.task,
             tcard: this.card,
-            edit: false
+            edit: (this.task.title == '')
         }
     },
     methods: {
@@ -29,8 +29,8 @@ Vue.component('card', {
     template: `
     <div class="card">
     <div class="row">
-        <div class="11 col" v-show="!edit" @click="edit=!edit && tcard.title != ''"><h4>{{ card.title }}</h4></div>
-        <div class="11 col" v-show="edit || tcard.title == ''"><input class="card w-100" v-model="tcard.title" @blur="edit=!edit"></div>
+        <div class="11 col" v-show="!edit" @click="edit=!edit"><h4>{{ card.title }}</h4></div>
+        <div class="11 col" v-show="edit"><input class="card w-100" v-model="tcard.title" @blur="edit=!edit"></div>
         <div class="1 col"><button class="btn primary" @click="addTask">Add task</button></div>
         <div class="1 col"><button class="btn primary">Remove Card</button></div>
     </div>
@@ -39,7 +39,7 @@ Vue.component('card', {
     data: function() {
         return {
             tcard: this.card,
-            edit: false
+            edit: (this.card.title == '')
         }
     },
     methods: {
